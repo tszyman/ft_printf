71e39e7c18f8e8e474d75a33189b91d4a8d4a123
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-05-09 22:05:57 +0200

    adding symbols as a function parameter

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 14:04:03 by tomek             #+#    #+#             */
-/*   Updated: 2024/05/08 18:48:19 by tomek            ###   ########.fr       */
+/*   Updated: 2024/05/08 20:45:21 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int	ft_printf(const char *format, ...);
 int	ft_printchar(int c);
 int	ft_printstr(char *str);
 int	ft_printp(void *p);
-int	ft_printnbr(long n, int base);
+int	ft_printnbr(long n, int base, char *symbols);
 
 #endif
--- ft_printnbr.c
+++ ft_printnbr.c
@@ -6,28 +6,26 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 18:54:37 by tomek             #+#    #+#             */
-/*   Updated: 2024/05/08 20:32:54 by tomek            ###   ########.fr       */
+/*   Updated: 2024/05/09 21:54:52 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printnbr(long n, int base)
+int	ft_printnbr(long n, int base, char *symbols)
 {
 	int		count;
-	char	*hex_symbols;
 
-	hex_symbols = "0123456789abcdef";
 	if (n < 0)
 	{
 		ft_printchar('-');
-		return (ft_printnbr(-n, base) + 1);
+		return (ft_printnbr(-n, base, symbols) + 1);
 	}
 	else if (n < base)
-		return (ft_printchar(hex_symbols[n]));
+		return (ft_printchar(symbols[n]));
 	else
 	{
-		count = ft_printnbr(n / base, base);
-		return count + ft_printnbr(n % base, base);
+		count = ft_printnbr(n / base, base, symbols);
+		return (count + ft_printnbr(n % base, base, symbols));
 	}
 }
